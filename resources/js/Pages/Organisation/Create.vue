49fc52b132f0a9ea7296ba2950afbac9e5590cc1
Holly Tippins <hollytippins@Mac-mini.local>
2023-01-17 17:20:17 +0000

    added styling to form

--- resources/js/Pages/Organisation/Create.vue
+++ resources/js/Pages/Organisation/Create.vue
@@ -1,23 +1,40 @@
 <template>
     <form @submit.prevent="create">
-        <div>
+        <div class="flex flex-col h-full w-1/2 mx-auto">
             <div>
-                <label>Name of organisation</label>
-                <input type="text" v-model="form.title" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Name of organisation</label
+                >
+                <input
+                    type="text"
+                    v-model="form.title"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
                 <div v-if="form.errors.title">
                     <p>Required field</p>
                 </div>
             </div>
             <div>
-                <label>Description</label>
-                <textarea type="text" v-model="form.description" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Description</label
+                >
+                <textarea
+                    type="text"
+                    v-model="form.description"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
                 <div v-if="form.errors.description">
                     <p>Required field</p>
                 </div>
             </div>
             <div>
-                <label>Category</label>
-                <select v-model="form.category">
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Category</label
+                >
+                <select
+                    v-model="form.category"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                >
                     <option disable value="">Please select...</option>
                     <option value="therapy">Therapy</option>
                     <option value="debt">Debt support</option>
@@ -52,8 +69,13 @@
                 </div>
             </div>
             <div>
-                <label>Demographics</label>
-                <select v-model="form.demographics">
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Demographics</label
+                >
+                <select
+                    v-model="form.demographics"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                >
                     <option disable value="">Please select...</option>
                     <option value="young person">Young Persons</option>
                     <option value="adult">Adults</option>
@@ -64,58 +86,111 @@
                 </div>
             </div>
             <div>
-                <label>Town/City</label>
-                <input type="text" v-model="form.city" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Town/City</label
+                >
+                <input
+                    type="text"
+                    v-model="form.city"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
                 <div v-if="form.errors.city">
                     <p>Required field</p>
                 </div>
             </div>
             <div>
-                <label>County</label>
-                <input type="text" v-model="form.county" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >County</label
+                >
+                <input
+                    type="text"
+                    v-model="form.county"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
                 <div v-if="form.errors.county">
                     <p>Required field</p>
                 </div>
             </div>
             <div>
-                <label>Phone number</label>
-                <input type="text" v-model.number="form.phone_num_1" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Phone number</label
+                >
+                <input
+                    type="text"
+                    v-model.number="form.phone_num_1"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
                 <div v-if="form.errors.phone_num_1">
                     <p>Error with input</p>
                 </div>
             </div>
             <div>
-                <label>Second phone number</label>
-                <input type="text" v-model.number="form.phone_num_2" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Second phone number (optional)</label
+                >
+                <input
+                    type="text"
+                    v-model.number="form.phone_num_2"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
                 <div v-if="form.errors.phone_num_2">
                     <p>Error with input</p>
                 </div>
             </div>
             <div>
-                <label>Text number</label>
-                <input type="text" v-model.number="form.text_num" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Text number (optional)</label
+                >
+                <input
+                    type="text"
+                    v-model.number="form.text_num"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
                 <div v-if="form.errors.text_num">
                     <p>Error with input</p>
                 </div>
             </div>
             <div>
-                <label>Website</label>
-                <input type="text" v-model="form.website" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Website (optional)</label
+                >
+                <input
+                    type="text"
+                    v-model="form.website"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
                 <div v-if="form.errors.website">
                     <p>Error with input</p>
                 </div>
             </div>
             <div>
-                <label>Email</label>
-                <input type="email" v-model="form.email" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Email (optional)</label
+                >
+                <input
+                    type="email"
+                    v-model="form.email"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
             </div>
             <div>
-                <label>Twitter</label>
-                <input type="text" v-model="form.twitter" />
+                <label class="block mb-1 text-gray-500 font-medium"
+                    >Twitter (optional)</label
+                >
+                <input
+                    type="text"
+                    v-model="form.twitter"
+                    class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500 mb-2"
+                />
             </div>
 
             <div>
-                <button type="submit">Submit</button>
+                <button
+                    type="submit"
+                    class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md w-full mt-2"
+                >
+                    Submit
+                </button>
             </div>
         </div>
     </form>
